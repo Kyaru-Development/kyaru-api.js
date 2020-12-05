b3ebd404e439be72791b3c96c3e04814f42b49e2
Kotik <62649860+XDENIX@users.noreply.github.com>
2020-12-05 15:54:17 +0300

    node-fetch

--- src/Base.js
+++ src/Base.js
@@ -1,37 +1,21 @@
-const http = require("http")
+const fetch = require("node-fetch")
  
 class KuaryAgent {
     constructor(key) {
         this.token = key;
-        if(!this.token) throw new Error("Specify API-KEY. Documentation - https://github.com/API-Sakura/Kyaru-API.JS")
+        if(!this.token) throw new Error("Specify API-KEY. Documentation - https://github.com/Kyaru-Development/kyaru-api.js")
 
-
-        this.request = function(endpoint, options){
-            return new Promise((resolve, reject) => {
-                http.get(`/v1/${endpoint}`, options, (res) => {
-                    let body = " ";
-                    res.on('data', function(chunk){
-                        body += chunk;
-                        return resolve(JSON.parse(body))
-                    })
-                })
-            })
-        }
  
-
-    }
-
     get = function(endpoint) {
-        if(!endpoint) throw new Error("Specify an available endpoint. Documentation -  https://github.com/API-Sakura/Kyaru-API.JS")
-        return this.request(endpoint, {
+        if(!endpoint) throw new Error("Specify an available endpoint. Documentation -  https://github.com/Kyaru-Development/kyaru-api.js")
+       let res = await fetch(`https://kyaru-api.glitch.me/ /api/v1/${endpoint}`, {
             headers: {
-                 "Authorization": this.token,
+                "Content-Type": "application/json",
+                "Authorization": this.token,
             },
         })
+        return res.json()
     }
-
 }
 
 module.exports = KuaryAgent;
-
-/* Developer with ❤ by Kyaru Development */
