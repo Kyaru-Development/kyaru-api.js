2afbcfb70070cb34ed75f9ba53c0772e5f3451d4
Kotik <62649860+XDENIX@users.noreply.github.com>
2020-12-05 15:54:49 +0300

    fix

--- src/Base.js
+++ src/Base.js
@@ -5,7 +5,7 @@ class KuaryAgent {
         this.token = key;
         if(!this.token) throw new Error("Specify API-KEY. Documentation - https://github.com/Kyaru-Development/kyaru-api.js")
 
- 
+    }
     get = function(endpoint) {
         if(!endpoint) throw new Error("Specify an available endpoint. Documentation -  https://github.com/Kyaru-Development/kyaru-api.js")
        let res = await fetch(`https://kyaru-api.glitch.me/ /api/v1/${endpoint}`, {
@@ -15,7 +15,7 @@ class KuaryAgent {
             },
         })
         return res.json()
-    }
+}
 }
 
 module.exports = KuaryAgent;
